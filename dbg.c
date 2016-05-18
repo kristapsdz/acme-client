903c7ca4273db89c1a3922dd4c1aaad28d5096ae
kristaps <>
2016-05-18 20:22:00 +0000

    Remove all logging in favour of warnx et al. Remove wrong-headed attempt at setproctitle.

--- dbg.c
+++ dbg.c
@@ -18,106 +18,22 @@
 # include "config.h"
 #endif
 
-#include <errno.h>
+#include <err.h>
 #include <stdarg.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 
 #include "extern.h"
-
-static	const char *const comps[COMP__MAX + 1] = {
-	"netproc", /* COMP_NET */
-	"keyproc", /* COMP_KEY */
-	"certproc", /* COMP_CERT */
-	"acctproc", /* COMP_ACCOUNT */
-	"challengeproc", /* COMP_CHALLENGE */
-	"fileproc", /* COMP_FILE */
-	"dnsproc", /* COMP_DNS */
-	"revokeproc", /* COMP_REVOKE */
-	"master", /* COMP__MAX */
-};
-
-static void
-dovddbg(const char *fmt, va_list ap)
-{
-	extern int	 verbose;
-	extern enum comp proccomp;
-
-	if (verbose < 2)
-		return;
-	fprintf(stderr, "%s(%u): TRACE: ", comps[proccomp], getpid());
-	vfprintf(stderr, fmt, ap);
-	fputc('\n', stderr);
-}
-
-static void
-dovdbg(const char *fmt, va_list ap)
-{
-	extern int	 verbose;
-	extern enum comp proccomp;
-
-	if ( ! verbose)
-		return;
-	fprintf(stderr, "%s(%u): DEBUG: ", comps[proccomp], getpid());
-	vfprintf(stderr, fmt, ap);
-	fputc('\n', stderr);
-}
-
-static void
-doverrx(const char *fmt, va_list ap)
-{
-	extern enum comp proccomp;
-
-	fprintf(stderr, "%s(%u): ERROR: ", comps[proccomp], getpid());
-	vfprintf(stderr, fmt, ap);
-	fputc('\n', stderr);
-	exit(EXIT_FAILURE);
-	/* NOTREACHED */
-}
-
-
-static void
-doverr(const char *fmt, va_list ap)
-{
-	int		 er = errno;
-	extern enum comp proccomp;
-
-	fprintf(stderr, "%s(%u): ERROR: ", comps[proccomp], getpid());
-	vfprintf(stderr, fmt, ap);
-	fprintf(stderr, ": %s\n", strerror(er));
-	exit(EXIT_FAILURE);
-	/* NOTREACHED */
-}
-
-void
-doerrx(const char *fmt, ...)
-{
-	va_list	 	 ap;
-
-	va_start(ap, fmt);
-	doverrx(fmt, ap);
-	va_end(ap);
-}
-
-void
-doerr(const char *fmt, ...)
-{
-	va_list	 	 ap;
-
-	va_start(ap, fmt);
-	doverr(fmt, ap);
-	va_end(ap);
-}
 
 void
 doddbg(const char *fmt, ...)
 {
 	va_list	 	 ap;
 
+	if (verbose < 2)
+		return;
+
 	va_start(ap, fmt);
-	dovddbg(fmt, ap);
+	vwarnx(fmt, ap);
 	va_end(ap);
 }
 
@@ -126,14 +42,10 @@ dodbg(const char *fmt, ...)
 {
 	va_list	 	 ap;
 
+	if ( ! verbose)
+		return;
+
 	va_start(ap, fmt);
-	dovdbg(fmt, ap);
+	vwarnx(fmt, ap);
 	va_end(ap);
-}
-
-const char *
-compname(enum comp comp)
-{
-
-	return(comps[comp]);
 }
