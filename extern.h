f9088e2289d5aab28f8520f18abc584524ad90d1
kristaps <>
2017-07-02 09:44:53 +0000

    Move last "low-hanging fruit" config ops to configuration.

--- extern.h
+++ extern.h
@@ -1,6 +1,6 @@
 /*	$Id$ */
 /*
- * Copyright (c) 2016 Kristaps Dzonsons <kristaps@bsd.lv>
+ * Copyright (c) 2016--2017 Kristaps Dzonsons <kristaps@bsd.lv>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -175,6 +175,7 @@ struct	capaths {
  * Configuration for a given domain and its altnames.
  */
 struct	config {
+	int		 newacct; /* generate new account key */
 	int		 newkey; /* generate new domain key */
 	int		 ocsp; /* request OCSP */
 	int		 revocate; /* revoke certificate */
@@ -191,7 +192,7 @@ __BEGIN_DECLS
  * Start with our components.
  * These are all isolated and talk to each other using sockets.
  */
-int		 acctproc(int, const char *, int);
+int		 acctproc(int, const char *, const struct config *);
 int		 certproc(int, int);
 int		 chngproc(int, const char *, const char *);
 int		 dnsproc(int);
@@ -200,7 +201,7 @@ int		 revokeproc(int, const char *,
 int		 fileproc(int, const char *, const struct config *);
 int		 keyproc(int, const char *,
 			const char **, size_t, const struct config *);
-int		 netproc(int, int, int, int, int, int, int, int,
+int		 netproc(int, int, int, int, int, int, int, 
 			const char *const *, size_t, const char *,
 			const char *, const struct config *);
 
